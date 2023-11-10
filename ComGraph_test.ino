bb0fdba3412e4b76f7ff22402bbff52bf5be858f
Sergei Potekhin <vexilurz@gmail.com>
2023-11-10 14:56:04 +0300

    first commit

---
+++ ComGraph_test.ino
@@ -0,0 +1,56 @@
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial) { }
+}
+
+void sendByte(int value)
+{
+  Serial.write(value);
+}
+
+void sendInt16(int value)
+{
+  sendByte(value);
+  sendByte(value >> 8);
+}
+
+void sendInt32(int value)
+{
+  sendInt16(value);
+  sendInt16(value >> 16);
+}
+
+void sendFloat(float value)
+{
+  sendInt32(*(int*)(&value));
+}
+
+int counter = 0;
+float fcounter = 0.0;
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    int inByte = Serial.read();
+    if (inByte == 9)
+    {
+      for (int i = 0; i < 1900; i++)
+      {
+        int m = millis();
+        sendInt32(m);
+        sendInt16(i);
+      }
+    }
+    if (inByte == 10)
+    {
+      int m = millis();
+      sendInt32(m);
+      sendByte(counter);
+      sendInt32(counter++);
+      sendFloat(fcounter);
+      fcounter += 0.1;
+    }
+  }
+}
